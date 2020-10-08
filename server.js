eac2d9243689fb9b8f5608627601c893e9125dea
samyev <sam1967evelyn@gmail.com>
2020-10-07 23:24:17 -0300

    adicionar nova rota \date ao arquivo server.js

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
-
 const app = express();
+//const date = express.date();
+
 
 app.get('/',(req, res) => {
-    res.send('Hello Batatinha!');
+    res.send('Good morning my dear <3');
+});
+
+
+app.get('/date',(req, res, next) => {
+    res.send("Today's date: (day 08) ~ (month 10) ~ (year 2020)");
+    next();
 });
 
 app.listen(3001);
