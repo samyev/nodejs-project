a36c00d023319ecfd6bd6298e1497aaf160b510c
samyev <sam1967evelyn@gmail.com>
2020-10-11 19:00:25 -0300

    adicionar porta e subir api para heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+require('dotenv').config();
 const requireDir = require("require-dir");
 
 // Inciciando o App
@@ -9,12 +10,16 @@ app.use(express.json());
 app.use(cors());
 
 //inicando o DB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mongodbwithnodejs.wpqij.mongodb.net/nodeapi?retryWrites=true&w=majority`
+
 mongoose.connect(
-    "mongodb://localhost:27017/nodeapi", 
+    uri, 
     { useUnifiedTopology: true ,
       useNewUrlParser: true }
 );
 requireDir('./src/models');
+
+var PORTA = process.env.PORT || 8080
 
 
 // Rotas
